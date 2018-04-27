0503c5f5480913b0b9f5b82ca44526bacd2c1d5a
anddopphoopha <andrew.dopphoopha@gmail.com>
2018-04-27 22:28:29 +0700

    add user test case 1, data file, and corresponding test suite

---
+++ Test Suites/Test Suite for User Test Case 1.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite for User Test Case 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-04-27T22:26:20</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3ddb126a-718d-447a-a902-996e4407f623</testSuiteGuid>
+   <testCaseLink>
+      <guid>af1e1f34-4f48-4e22-87e8-6a35b3793b6d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Test Case 1</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>1b85c710-ef3d-40fa-85af-10edea329245</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Test Data for User Test Case 1</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>1b85c710-ef3d-40fa-85af-10edea329245</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>d902e09a-652c-418a-bb53-4bb051c0c09f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>1b85c710-ef3d-40fa-85af-10edea329245</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>936ddeb0-fa6e-4c6c-9d00-0177e0709876</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
