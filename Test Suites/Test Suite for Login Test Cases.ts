722b2c9aa0b525e738b84bb95f3c1e16f16b2ab9
anddopphoopha <andrew.dopphoopha@gmail.com>
2018-04-27 22:06:22 +0700

    create test suite for login cases

--- Test Suites/Test Suite for Login Test Cases.ts
+++ Test Suites/Test Suite for Login Test Cases.ts
@@ -4,6 +4,7 @@
    <name>Test Suite for Login Test Cases</name>
    <tag></tag>
    <isRerun>false</isRerun>
+   <lastRun>2018-04-27T22:02:40</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
