82d9a15c89eaaf2c0e69babf5e3bca4489d9c663
anddopphoopha <andrew.dopphoopha@gmail.com>
2018-05-08 11:56:17 +0700

    Edit Test Suite for Login Test Case for Empty Username
    Added edited test data and test case

--- Test Suites/Test Suite for Login Test Case for Empty Username.ts
+++ Test Suites/Test Suite for Login Test Case for Empty Username.ts
@@ -4,7 +4,7 @@
    <name>Test Suite for Login Test Case for Empty Username</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-04-30T22:02:32</lastRun>
+   <lastRun>2018-05-08T11:50:17</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
@@ -20,7 +20,7 @@
          <combinationType>ONE</combinationType>
          <id>62c8c41d-3a0f-4ca2-9bd6-77e875820889</id>
          <iterationEntity>
-            <iterationType>RANGE</iterationType>
+            <iterationType>ALL</iterationType>
             <value>3-3</value>
          </iterationEntity>
          <testDataId>Data Files/Test Data for Login Test Case for Empty Username</testDataId>
@@ -30,6 +30,12 @@
          <type>DATA_COLUMN</type>
          <value>username</value>
          <variableId>d1adf43a-f9d9-417d-a72a-f768f1211228</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>62c8c41d-3a0f-4ca2-9bd6-77e875820889</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>48543986-b451-4c6c-8ac0-0746218a8b5d</variableId>
       </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
